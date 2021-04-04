3c46f0823d373e26083c8cde90f52512295c89c9
ric-satelite <ric-satelite@localhost.localdomain>
2021-04-04 07:33:38 -0400

    testing purposes

--- init.h
+++ init.h
@@ -1,6 +1,10 @@
 #include <time.h>
 #define RTP_HEADER_LEN 12 // bytes
 
+// Error codes
+#define VALID_HEADER 0
+#define INVALID_HEADER 1
+
 int init_connection(char host[], uint16_t port)
 {
   struct sockaddr_in connection_address;
@@ -42,62 +46,74 @@ typedef struct {
 } RTP_HEADER;
 
 // RTP
-RTP_HEADER* ParseRTPHeader(char* packet)
+int ParseRTPHeader(RTP_HEADER* header, char* packet)
 {
 
-  RTP_HEADER header;
-
   // Version (supported specification is 2)
-  if( !(packet[0]&0x80) ) return -1;
+  if( !(packet[0]&0x80) ) return INVALID_HEADER;
+  else {
 
-  // Padding bit
-  header.padding = (packet[0]>>5) - 0b100;
+    // Padding bit
+    header->padding = (packet[0]>>5) - 0b100;
 
-  // CSRC count (CC)
-  header.csrc_count = (packet[0]<<4);
-  csrc_count=csrc_count>>4;
+    // CSRC count (CC)
+    header->csrc_count = (packet[0]<<4);
+    header->csrc_count=header->csrc_count>>4;
 
-  // Marker bit
-  header.marker = packet[1]>>7;
+    // Marker bit
+    header->marker = packet[1]>>7;
 
-  // Payload type: ignore for now
+    // Payload type: ignore for now
 
-  header.sequence_number = packet[2];
-  sequence_number = sequence_number<<8;
-  sequence_number += packet[3];
+    header->sequence_number = packet[2];
+    header->sequence_number = header->sequence_number<<8;
+    header->sequence_number += packet[3];
 
-  header.timestamp = packet[4];
-  timestamp = (timestamp<<24);
-  timestamp += (0x00+packet[5])<<16;
-  timestamp += (0x00+packet[6])<<8;
-  timestamp += packet[7];
+    header->timestamp = packet[4];
+    header->timestamp = (header->timestamp<<24);
+    header->timestamp += (0x00+packet[5])<<16;
+    header->timestamp += (0x00+packet[6])<<8;
+    header->timestamp += packet[7];
 
-  // SSRC and CSRC list is not implemented
+    // SSRC and CSRC list is not implemented
+  }
 
-  return &header;
+  return VALID_HEADER;
 }
 
 
 // Currently only support Fragmented Units to be processed
 // process: ( FU indicator  and   FU header )
-int ParseH264Header(char* packet)
+typedef struct {
+  unsigned char type;
+  unsigned char boundary_bit;
+} H264Header;
+
+int ParseH264Header(H264Header* header, char* packet)
 {
   // FU indicator
   unsigned char forbidden_zero_bit = packet[0]>>7;
-  if(forbidden_zero_bit) return -1; // drop packet
+  if(forbidden_zero_bit) return INVALID_HEADER; // drop packet
 
-  unsigned char type = packet[0]<<3;
-  type = type>>3;
+  header->type = packet[0]<<3;
+  header->type = header->type>>3;
 
-  if( type == 28 ); // FU-A
-  else if( type == 29 ); // FU-B | not supported
+  if(header->type == 7 || header->type == 8)
+  {
+    header->boundary_bit = 0;
+  }
 
-  // Fu header
-  unsigned char boundary_bit = 0; // 1 when start indicator, 2 when end indicator is set
-  if( (packet[1]&0x80) ) boundary_bit = 1;
-  else if( (packet[1]&0x40) ) boundary_bit = 2;
+  else if( header->type == 28 ) // FU-A
+  {
+    // Fu header
+    header->boundary_bit = 0; // 1 when start indicator, 2 when end indicator is set
+    if( (packet[1]&0x80) ) header->boundary_bit = 1;
+    else if( (packet[1]&0x40) ) header->boundary_bit = 2;
+  }
 
-  return type
+  else if( header->type == 29 ); // FU-B | not supported
+
+  return VALID_HEADER;
 }
 
 bridge* camera_feed_bridge()
@@ -147,30 +163,66 @@ bridge* camera_feed_bridge()
 
   if(fork() == 0) {
 
-    int PC = 20;
-    char* stream_data = (char*)malloc(6557);
+    char* video_stream = (char*)malloc(65534*35);
+    char* stream_data = (char*)malloc(65535); // stream data here means NALU
+    RTP_HEADER rtp_header;
+    H264Header h264_header;
 
-    while(PC) {
-      memset(stream_data, 6557, 0);
-      int recv_len;
+    int recv_len, stream_pointer = 4, valid_packet_started = 0;
+
+    printf("%d %d\n", stream_pointer, valid_packet_started);
+
+    // NOTE: there might be padding indicated by the RTP header properties
+    while(1) {
+      memset(stream_data, 65535, 0);
 
       // recv_len = recvfrom(sockfd, stream_data, 6556, 0, (struct sockaddr*)&camera_client, (socklen_t*)sizeof(camera_client));
-      recv_len = recvfrom(sockfd, stream_data, 6556, 0, NULL, NULL);
+      recv_len = recvfrom(sockfd, stream_data, 65534, 0, NULL, NULL);
       if(recv_len < 8) puts("RTP header is missing!");
-      ParseRTPHeader(stream_data);
-      ParseH264Header(stream_data+RTP_HEADER_LEN);
 
-      PC--;
+      if(ParseRTPHeader(&rtp_header, stream_data) != 0) continue;
+      if(ParseH264Header(&h264_header, stream_data+RTP_HEADER_LEN) != 0 ) continue;
+
+      // if(h264_header.type == 7) puts("fadasdas --------- ");
+      // if(h264_header.type != 28 || h264_header.type != 7 || h264_header.type != 8) continue;
+      if(h264_header.type != 28 && h264_header.type != 7 && h264_header.type != 8) continue;
+      printf("%d %d type -------- \n", h264_header.type, h264_header.boundary_bit);
+
+      if(h264_header.boundary_bit == 1 && !valid_packet_started) {
+        valid_packet_started = 1;
+        memset(video_stream, 65535*35, 0);
+        video_stream[3] = 0x1;
+
+        puts("Started valid");
+      }
+
+      if(valid_packet_started) {
+
+        puts("valid_packet_started");
+        // memcpy(video_stream+stream_pointer, stream_data+RTP_HEADER_LEN+2, recv_len);
+
+        #define DEC RTP_HEADER_LEN
+        memcpy(video_stream+stream_pointer, stream_data+DEC, recv_len-DEC);
+        stream_pointer += recv_len-DEC-4;
+
+        if(h264_header.boundary_bit == 2) {
+          puts("end of boundary");
+          valid_packet_started = 0;
+          FILE* f = fopen("img.bin", "wb");
+          fwrite(video_stream, 1, stream_pointer, f);
+          fclose(f);
+          break;
+        }
+      }
+
+      // break;
+
     }
 
-    // DEBUG:
-    // printf("%d\n", recv_len);
-    // for(int i=0;i<recv_len;i++) {
-      // printf("%#.8x\n", stream_data[i]);
-    // }
-    // printf("%p %#.8X %d %d\n", stream_data[1], stream_data[1], recv_len, sockfd);
-
     free(stream_data);
+    free(video_stream);
+
+    puts("END");
     exit(0);
   }
 
